778dca4e846279c0777ea03eae47d1d26a3ff4d2
AlvaroManasses <alvaromlsilva@gmail.com>
2024-08-02 00:04:18 -0300

    add file createWallet.js
    Script em Node.js usado para gerar endereços de Bitcoin usando a rede Testnet.

--- Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
+++ Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
@@ -4,30 +4,32 @@ const bip39 = require('bip39')
 const bitcoin = require('bitcoinjs-lib')
 
 //definir a rede
-//bitcoin - rede principal - mainnet
-//testnet - rede de teste - tesnet
-const network = bitcoin.networks.testnet
+const network = bitcoin.networks.testnet // configura a rede para teste
+//const network = bitcoin.networks.bitcoin configura a rede para principal
 
 //derivação de carteiras HD
-const path = `m/49'/1'/0'/0` 
+const path = `m/49'/1'/0'/0` // caminho na testnet
+//const path = `m/49'/0'/0'/0` // caminho na mainnet
 
 //criando o mnemonic para a seed (palavras de senha)
 let mnemonic = bip39.generateMnemonic()
 const seed = bip39.mnemonicToSeedSync(mnemonic)
 
-//criando a raiz da cartiera HD
+//criando a raiz da carteira HD
 let root = bip32.fromSeed(seed, network)
 
-//criando uma conta - par pvt-pub keys
+//criando uma conta - par private-public keys
 let account = root.derivePath(path)
 let node = account.derive(0).derive(0)
 
-let btcAddress = bitcoin.payments.p2pkh({
+let btcAddresss = bitcoin.payments.p2pkh({
     pubkey: node.publicKey,
     network: network,
 }).address
 
 console.log("Carteira gerada")
-console.log("Endereço: ", btcAddress)
-console.log("Chave privada:", node.toWIF())
-console.log("Seed:", mnemonic)
+console.log("Endereço: ", btcAddresss)
+console.log("Chave privada:", node.toWIF()) // exibe no formato usado pelo nosso gerenciador de carteiras
+console.log("Seed", mnemonic)
+
+
